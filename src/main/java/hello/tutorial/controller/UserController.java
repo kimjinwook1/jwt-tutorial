37d48869012dac8f7c8a32089b17ebf8cddf8540
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-20 17:39:08 +0900

    회원가입, 권한검증

---
+++ src/main/java/hello/tutorial/controller/UserController.java
@@ -0,0 +1,40 @@
+package hello.tutorial.controller;
+
+import hello.tutorial.dto.UserDto;
+import hello.tutorial.service.UserService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<UserDto> signup(@Valid @RequestBody UserDto userDto) {
+		return ResponseEntity.ok(userService.signup(userDto));
+	}
+
+	@GetMapping("/user")
+	@PreAuthorize("hasAnyRole('USER','ADMIN')")
+	public ResponseEntity<UserDto> getMyUserInfo() {
+		return ResponseEntity.ok(userService.getMyUserWithAuthorities());
+	}
+
+	@GetMapping("/user/{username}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<UserDto> getUserInfo(@PathVariable("username") String username) {
+		return ResponseEntity.ok(userService.getUserWithAuthorities(username));
+	}
+}
+
---
+++ src/main/java/hello/tutorial/service/UserService.java
@@ -0,0 +1,51 @@
+package hello.tutorial.service;
+
+import hello.tutorial.dto.UserDto;
+import hello.tutorial.entity.Authority;
+import hello.tutorial.entity.User;
+import hello.tutorial.repository.UserRepository;
+import hello.tutorial.util.SecurityUtil;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public UserDto signup(UserDto userDto) {
+		if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+			throw new RuntimeException("이미 가입되어 있는 유저 입니다.");
+		}
+
+		Authority authority = Authority.builder()
+				.authorityName("ROLE_USER")
+				.build();
+
+		User user = User.builder()
+				.username(userDto.getUsername())
+				.password(passwordEncoder.encode(userDto.getPassword()))
+				.nickname(userDto.getNickname())
+				.authorities(Collections.singleton(authority))
+				.activated(true)
+				.build();
+
+		return UserDto.from(userRepository.save(user));
+	}
+
+	public UserDto getUserWithAuthorities(String username) {
+		return UserDto.from(userRepository.findOneWithAuthoritiesByUsername(username).orElse(null));
+	}
+
+	public UserDto getMyUserWithAuthorities() {
+
+		return UserDto.from(SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername).orElse(null));
+	}
+}
