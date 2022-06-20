074e4a7f37cd8de269707f29710aaec408f43f27
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-20 14:04:47 +0900

    Security 설정, Data 설정

---
+++ src/main/java/hello/tutorial/entity/Authority.java
@@ -0,0 +1,24 @@
+package hello.tutorial.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+	@Id
+	@Column(name = "authority_name", length = 50)
+	private String authorityName;
+}
